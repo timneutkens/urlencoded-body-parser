2f11064e3448fdeddde47e90e89226223c3e6ad5
chris <thisguychriz@gmail.com>
2017-03-28 07:28:30 +0800

    Add fallback for encoding
    - encoding fallbacks to `UTF-8`
    - getting TypeError in media typer when content type is missing
    - related to https://github.com/zeit/micro/pull/208

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { parse } = require('qs')
 
 module.exports = async function parseFormData (req, {limit = '1mb'} = {}) {
   const type = req.headers['content-type']
-  const encoding = typer.parse(type).parameters.charset
+  const encoding = (type) ? typer.parse(type).parameters.charset : 'UTF-8'
 
   req.rawBody = req.rawBody || getRawBody(req, {limit, encoding})
   const str = await req.rawBody
